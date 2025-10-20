e80b9ab95075b64b56b54f82781640f34562d0f4
adeel26in <mohammadhammad18032012@gmail.com>
2025-10-20 13:06:03 +0200

    Merge pull request #369 from adeel26in/main
    Duplicate purge in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    purge: [],
     purge: ['./src/renderer/index.html', './src/renderer/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
