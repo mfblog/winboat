a4228ce70e542be7a29e1d0b174ce50c6138080f
adeel26in <mohammadhammad18032012@gmail.com>
2025-10-25 14:37:28 +0200

    chore: Update tailwind.config.ts (#405)
    Updated "purge" to "content"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    purge: ["./src/renderer/index.html", "./src/renderer/**/*.{vue,js,ts,jsx,tsx}"],
+    content: ["./src/renderer/index.html", "./src/renderer/**/*.{vue,js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
